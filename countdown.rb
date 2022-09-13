3a32b1a2916cd500dbb1df68cec383224e1d54f8
HFelixH <felix-hoang@outlook.com>
2022-09-13 16:13:20 +0930

    commit tree a2e8aa7ba601906ecc251a7d0e0f22370098b8bf

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
 #write your code here
 
-def countdown
+def countdown(seconds_to_new_year)
+    while seconds_to_new_year > 0
+        puts "#{seconds_to_new_year} SECOND(S)!"
+        seconds_to_new_year -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
